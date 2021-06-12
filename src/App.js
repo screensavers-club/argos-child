92af9e5b33b5e2c15eeb21d4c5d2997325f1fe73
Siah <i@siah.sg>
2021-06-13 02:28:08 +0800

    feat: wip

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+import Peer from "peerjs";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	let [conn, setConn] = useState({});
+	useEffect(() => {
+		let peer = new Peer({
+			host: "192.168.1.33",
+			port: "9000",
+			debug: 2,
+			path: "/peerjs/myapp",
+		});
+
+		peer.on("open", (id) => {
+			console.log("my id is " + id);
+			let _c = peer.connect("parent");
+			setConn(_c);
+
+			_c.on("open", () => {
+				console.log("connection opened");
+				_c.send("hi");
+			});
+
+			_c.on("data", (data) => {
+				console.log("received ", data);
+			});
+
+			return function cleanup() {
+				peer.disconnect();
+			};
+		});
+	}, []);
+
+	return <div className="App">{console.log(conn)}</div>;
 }
 
 export default App;
