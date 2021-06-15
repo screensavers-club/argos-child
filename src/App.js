72d35d3f4fc7b292b3adc0172b377e39947c736f
Siah <i@siah.sg>
2021-06-15 20:14:11 +0800

    wip

--- src/App.js
+++ src/App.js
@@ -1,38 +1,110 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import "./App.css";
 import Peer from "peerjs";
 
 function App() {
-	let [conn, setConn] = useState({});
-	useEffect(() => {
-		let peer = new Peer({
-			host: "192.168.1.33",
-			port: "9000",
-			debug: 2,
-			path: "/peerjs/myapp",
-		});
+  let [peer, setPeer] = useState(false);
+  let [conn, setConn] = useState();
+  let [mediaStream, setMediaStream] = useState(false);
+  let [connectedToPeer, setConnectedToPeer] = useState();
+  let [connectedToParent, setConnectedToParent] = useState(false);
+  let videoRef = useRef();
 
-		peer.on("open", (id) => {
-			console.log("my id is " + id);
-			let _c = peer.connect("parent");
-			setConn(_c);
+  function callParent() {
+    if (!peer) {
+      return;
+    }
 
-			_c.on("open", () => {
-				console.log("connection opened");
-				_c.send("hi");
-			});
+    peer.call("parent", mediaStream);
+  }
 
-			_c.on("data", (data) => {
-				console.log("received ", data);
-			});
+  function connectToParent() {
+    let _conn = peer.connect("parent");
+    setConn(_conn);
 
-			return function cleanup() {
-				peer.disconnect();
-			};
-		});
-	}, []);
+    _conn.on("open", () => {
+      console.log("connected");
+      setConnectedToParent(true);
+    });
 
-	return <div className="App">{console.log(conn)}</div>;
+    _conn.on("close", () => {
+      setConnectedToParent(false);
+    });
+  }
+
+  function startVideo() {
+    navigator.mediaDevices
+      .getUserMedia({ audio: true, video: true })
+      .then((stream) => {
+        setMediaStream(stream);
+        videoRef.current.srcObject = stream;
+        // videoRef.current.play();
+      })
+      .catch((err) => {
+        alert(err);
+      });
+  }
+
+  useEffect(() => {
+    let _peer = new Peer({
+      // host: "localhost",
+      host: "192.168.86.204",
+      port: "9000",
+      debug: 2,
+      path: "/peerjs/myapp",
+    });
+    setPeer(_peer);
+
+    _peer.on("open", (id) => {
+      // console.log("my id is " + id);
+      setConnectedToPeer(true);
+    });
+
+    _peer.on("close", () => {
+      setConnectedToPeer(false);
+    });
+
+    _peer.on("disconnected", () => {
+      setConnectedToPeer(false);
+    });
+
+    return function cleanup() {
+      _peer.disconnect();
+    };
+  }, []);
+
+  return (
+    <div className="App">
+      {mediaStream ? (
+        <video ref={videoRef} autoPlay={true} muted={true} />
+      ) : (
+        <>
+          <button onClick={startVideo}>Start Video</button>
+        </>
+      )}
+      <br />
+
+      {connectedToPeer ? (
+        connectedToParent ? (
+          mediaStream && connectedToParent ? (
+            <button onClick={callParent}>Start Transmitting</button>
+          ) : (
+            <></>
+          )
+        ) : (
+          <button onClick={connectToParent}>Connect To Parent Node</button>
+        )
+      ) : (
+        <button
+          onClick={() => {
+            peer.reconnect();
+          }}
+        >
+          Reconnect
+        </button>
+      )}
+    </div>
+  );
 }
 
 export default App;
