0439bd79adaa17384dadc43f223c18024b8f29c9
Siah <i@siah.sg>
2021-09-10 18:46:07 +0800

    wip: room event listener for request layout

--- src/pages/stage.js
+++ src/pages/stage.js
@@ -1,361 +1,354 @@
 import styled from "styled-components";
 import {
-  useRoom,
-  useParticipant,
-  VideoRenderer,
-  AudioRenderer,
+	useRoom,
+	useParticipant,
+	VideoRenderer,
+	AudioRenderer,
 } from "livekit-react";
 import {
-  createLocalAudioTrack,
-  createLocalVideoTrack,
-  RoomEvent,
-  DataPacket_Kind,
+	createLocalAudioTrack,
+	createLocalVideoTrack,
+	RoomEvent,
+	DataPacket_Kind,
 } from "livekit-client";
 import { useEffect, useRef, useState } from "react";
 import {
-  Microphone,
-  Mute,
-  Exit,
-  Film,
-  Phone,
-  VolumeOff,
-  Controls,
-  EyeCrossed,
+	Microphone,
+	Mute,
+	Exit,
+	Film,
+	Phone,
+	VolumeOff,
+	Controls,
+	EyeCrossed,
 } from "react-ikonate";
 
 const StageDiv = styled.div`
-  position: fixed;
-  display: block;
-  width: 100%;
-  height: 100%;
+	position: fixed;
+	display: block;
+	width: 100%;
+	height: 100%;
 
-  div.streamTabs {
-    display: flex;
-    justify-content: space-around;
-    width: 80%;
-    height: 3em;
-    position: absolute;
-    bottom: 5em;
-    left: 50%;
-    transform: translate(-50%, 0);
-    border: 1px solid black;
-    border-radius: 0.5em;
+	div.streamTabs {
+		display: flex;
+		justify-content: space-around;
+		width: 80%;
+		height: 3em;
+		position: absolute;
+		bottom: 5em;
+		left: 50%;
+		transform: translate(-50%, 0);
+		border: 1px solid black;
+		border-radius: 0.5em;
 
-    > button {
-      width: 100%;
-      height: 100%;
-      padding: 0.5em 0;
-      appearance: none;
-      border: none;
-      border-right: 1px solid black;
-      font-size: 1.5em;
+		> button {
+			width: 100%;
+			height: 100%;
+			padding: 0.5em 0;
+			appearance: none;
+			border: none;
+			border-right: 1px solid black;
+			font-size: 1.5em;
 
-      /* background:${(p) => (!p.selected ? "black" : "white")} */
+			/* background:${(p) => (!p.selected ? "black" : "white")} */
 
-      &:hover {
-        background: #ddd;
+			&:hover {
+				background: #ddd;
 
-        &.active {
-          background: #888;
-        }
-      }
+				&.active {
+					background: #888;
+				}
+			}
 
-      &.active {
-        background: #aaa;
-      }
+			&.active {
+				background: #aaa;
+			}
 
-      svg {
-        stroke-linecap: round;
-        stroke-width: 1.5;
-      }
+			svg {
+				stroke-linecap: round;
+				stroke-width: 1.5;
+			}
 
-      &:first-child {
-        border-top-left-radius: 0.3em;
-        border-bottom-left-radius: 0.3em;
-      }
+			&:first-child {
+				border-top-left-radius: 0.3em;
+				border-bottom-left-radius: 0.3em;
+			}
 
-      &:last-child {
-        border: none;
-        border-top-right-radius: 0.3em;
-        border-bottom-right-radius: 0.3em;
-      }
-    }
-  }
+			&:last-child {
+				border: none;
+				border-top-right-radius: 0.3em;
+				border-bottom-right-radius: 0.3em;
+			}
+		}
+	}
 `;
 
 const VideoGrid = styled.div`
-  display: flex;
-  height: 70%;
+	display: flex;
+	height: 70%;
 
-  label.participantNumber {
-    position: absolute;
-    font-size: 5em;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
-  }
+	label.participantNumber {
+		position: absolute;
+		font-size: 5em;
+		top: 50%;
+		left: 50%;
+		transform: translate(-50%, -50%);
+	}
 
-  > div {
-    position: relative;
-    display: block;
-    justify-self: stretch;
-    align-self: stretch;
+	> div {
+		position: relative;
+		display: block;
+		justify-self: stretch;
+		align-self: stretch;
 
-    span {
-      position: absolute;
-      bottom: 0;
-      right: 0;
-      background: #000;
-      font-size: 0.5em;
-      color: white;
-    }
-  }
+		span {
+			position: absolute;
+			bottom: 0;
+			right: 0;
+			background: #000;
+			font-size: 0.5em;
+			color: white;
+		}
+	}
 
-  > div.remote-participant {
-    position: relative;
-    display: flex;
-    width: ${(p) => {
-      let COL_COUNT = [
-        1, 2, 2, 2, 3, 3, 3, 3, 3, 4, 4, 4, 4, 4, 4, 4, 5, 5, 5, 5, 5, 5, 5, 5,
-        5, 6, 6, 6, 6, 6,
-      ];
-      let numP = p.participants.filter((p) => !p.isLocal).length;
-      return 100 / COL_COUNT[numP - 1];
-    }}%;
-    order: 2;
+	> div.remote-participant {
+		position: relative;
+		display: flex;
+		width: ${(p) => {
+			let COL_COUNT = [
+				1, 2, 2, 2, 3, 3, 3, 3, 3, 4, 4, 4, 4, 4, 4, 4, 5, 5, 5, 5, 5, 5, 5, 5,
+				5, 6, 6, 6, 6, 6,
+			];
+			let numP = p.participants.filter((p) => !p.isLocal).length;
+			return 100 / COL_COUNT[numP - 1];
+		}}%;
+		order: 2;
 
-    video {
-      position: absolute;
-      object-fit: contain;
-      width: 100%;
-      height: 100%;
-    }
-  }
+		video {
+			position: absolute;
+			object-fit: contain;
+			width: 100%;
+			height: 100%;
+		}
+	}
 
-  div.participants {
-    border: 1px solid black;
-  }
+	div.participants {
+		border: 1px solid black;
+	}
 
-  > div.local-participant {
-    position: fixed;
-    width: 50%;
-    top: 50%;
-    left: 0;
-    transform: translate(0, -50%);
+	> div.local-participant {
+		position: fixed;
+		width: 50%;
+		top: 50%;
+		left: 0;
+		transform: translate(0, -50%);
 
-    video {
-      object-fit: contain;
-      width: 100%;
-      height: 100%;
-      border: 1px solid black;
-    }
-  }
+		video {
+			object-fit: contain;
+			width: 100%;
+			height: 100%;
+			border: 1px solid black;
+		}
+	}
 `;
 
 export default function Stage({ send, context, state, tabs }) {
-  const { connect, isConnecting, room, error, participants, audioTracks } =
-    useRoom();
+	const { connect, isConnecting, room, error, participants, audioTracks } =
+		useRoom();
 
-  function getCurrentLayout() {
-    return context.current_layout;
-  }
+	function getCurrentLayout() {
+		return context.current_layout;
+	}
 
-  // console.log(connect);
-  // console.log(room);
+	const encoder = new TextEncoder();
+	const decoder = new TextDecoder();
 
-  const [localVideoTrack, setLocalVideoTrack] = useState();
+	const [localVideoTrack, setLocalVideoTrack] = useState();
 
-  const [renderState, setRenderState] = useState(0);
+	const [renderState, setRenderState] = useState(0);
 
-  const localAudioTrackRef = useRef(null);
-  const localVideoTrackRef = useRef(null);
+	const localAudioTrackRef = useRef(null);
+	const localVideoTrackRef = useRef(null);
 
-  // room.localParticipant.publishTrack(localVideoTrackRef.current);
+	function sendCurrentLayout(recipient) {
+		if (room) {
+			const strData = JSON.stringify({
+				current_layout: context.current_layout,
+			});
+			const data = encoder.encode(strData);
+			room.localParticipant.publishData(data, DataPacket_Kind.RELIABLE, [
+				recipient,
+			]);
+		}
+	}
 
-  useEffect(async () => {
-    connect(process.env.REACT_APP_LIVEKIT_SERVER, context.token).then(
-      (_room) => {
-        const encoder = new TextEncoder();
-        const decoder = new TextDecoder();
-        console.log(context);
-        send("INIT_LAYOUT_WITH_SELF", { sid: _room.localParticipant.sid });
+	if (room) {
+		room.on(RoomEvent.DataReceived, (payload, participant) => {
+			const payloadStr = decoder.decode(payload);
+			const payloadObj = JSON.parse(payloadStr);
 
-        console.log(context);
-        _room.on(RoomEvent.DataReceived, (payload, participant) => {
-          const payloadStr = decoder.decode(payload);
-          const payloadObj = JSON.parse(payloadStr);
+			const requesterSid = participant.sid;
 
-          const requesterSid = participant.sid;
+			if (payloadObj.action === "REQUEST_CURRENT_LAYOUT") {
+				sendCurrentLayout(requesterSid);
+			}
+		});
+	}
 
-          switch (payloadObj.action) {
-            case "REQUEST_CURRENT_LAYOUT":
-              const strData = JSON.stringify({
-                current_layout: getCurrentLayout(),
-              });
-              const data = encoder.encode(strData);
-              _room.localParticipant.publishData(
-                data,
-                DataPacket_Kind.RELIABLE,
-                [requesterSid]
-              );
-              break;
-            default:
-              break;
-          }
-        });
-      }
-    );
-    // _room.on(RoomEvent.TrackPublished, handleNewTrack);
-    return () => {
-      // console.log("disconnecting");
-      room.disconnect();
-      // room.localParticipant.publishTrack(localVideoTrackRef.current);
-    };
-  }, []);
+	useEffect(() => {
+		connect(process.env.REACT_APP_LIVEKIT_SERVER, context.token).then(
+			(_room) => {
+				send("INIT_LAYOUT_WITH_SELF", { sid: _room.localParticipant.sid });
+			}
+		);
+		return () => {
+			room.disconnect();
+		};
+	}, []);
 
-  return (
-    <StageDiv selected={localVideoTrackRef.current}>
-      <VideoGrid participants={participants}>
-        {participants
-          .reduce((p, c) => {
-            if (!p.find((_p) => _p.identity === c.identity)) {
-              p.push(c);
-            }
-            return p;
-          }, [])
-          .map((participant, i, arr) => {
-            return (
-              <Participant
-                participant={participant}
-                participantNumber={i}
-                totalParticipants={arr.length}
-              />
-            );
-          })}
-      </VideoGrid>
-      <br />
+	return (
+		<StageDiv selected={localVideoTrackRef.current}>
+			<VideoGrid participants={participants}>
+				{participants
+					.reduce((p, c) => {
+						if (!p.find((_p) => _p.identity === c.identity)) {
+							p.push(c);
+						}
+						return p;
+					}, [])
+					.map((participant, i, arr) => {
+						return (
+							<Participant
+								participant={participant}
+								participantNumber={i}
+								totalParticipants={arr.length}
+							/>
+						);
+					})}
+			</VideoGrid>
+			<br />
 
-      <div className="streamTabs">
-        {(tabs = [
-          {
-            tab: "mic",
-            icon: !localAudioTrackRef.current ? <Microphone /> : <Mute />,
-            onClick: async () => {
-              if (localAudioTrackRef.current) {
-                room.localParticipant.unpublishTrack(
-                  localAudioTrackRef.current
-                );
-                localAudioTrackRef.current = null;
-              } else {
-                localAudioTrackRef.current =
-                  await createLocalAudioTrack().catch((err) => {
-                    alert(err);
-                  });
-                if (localAudioTrackRef.current) {
-                  room.localParticipant.publishTrack(
-                    localAudioTrackRef.current
-                  );
-                }
-              }
-              setRenderState(renderState + 1);
-            },
-          },
+			<div className="streamTabs">
+				{(tabs = [
+					{
+						tab: "mic",
+						icon: !localAudioTrackRef.current ? <Microphone /> : <Mute />,
+						onClick: async () => {
+							if (localAudioTrackRef.current) {
+								room.localParticipant.unpublishTrack(
+									localAudioTrackRef.current
+								);
+								localAudioTrackRef.current = null;
+							} else {
+								localAudioTrackRef.current =
+									await createLocalAudioTrack().catch((err) => {
+										alert(err);
+									});
+								if (localAudioTrackRef.current) {
+									room.localParticipant.publishTrack(
+										localAudioTrackRef.current
+									);
+								}
+							}
+							setRenderState(renderState + 1);
+						},
+					},
 
-          {
-            tab: "volume",
-            icon: <VolumeOff />,
-          },
-          {
-            tab: "video",
-            icon: localVideoTrackRef.current ? <Film /> : <EyeCrossed />,
-            onClick: async () => {
-              // console.log(localVideoTrackRef.current);
-              if (localVideoTrackRef.current) {
-                room.localParticipant.unpublishTrack(
-                  localVideoTrackRef.current
-                );
-                localVideoTrackRef.current = null;
-              } else {
-                localVideoTrackRef.current =
-                  await createLocalVideoTrack().catch((err) => {
-                    alert(err);
-                  });
-                if (localVideoTrackRef.current) {
-                  room.localParticipant.publishTrack(
-                    localVideoTrackRef.current
-                  );
-                }
-              }
-              setLocalVideoTrack(localVideoTrackRef.current);
-            },
-          },
-          {
-            tab: "end",
-            icon: <Exit />,
-            onClick: () => {
-              room?.disconnect();
-              send("DISCONNECT");
-            },
-          },
-          { tab: "call", icon: <Phone /> },
-          { tab: "controls", icon: <Controls /> },
-        ]).map(function ({ tab, icon, onClick }, i) {
-          let key = `key_${i}`;
-          return (
-            <button key={key} onClick={onClick}>
-              {icon}
-            </button>
-          );
-        })}
-      </div>
-    </StageDiv>
-  );
+					{
+						tab: "volume",
+						icon: <VolumeOff />,
+					},
+					{
+						tab: "video",
+						icon: localVideoTrackRef.current ? <Film /> : <EyeCrossed />,
+						onClick: async () => {
+							// console.log(localVideoTrackRef.current);
+							if (localVideoTrackRef.current) {
+								room.localParticipant.unpublishTrack(
+									localVideoTrackRef.current
+								);
+								localVideoTrackRef.current = null;
+							} else {
+								localVideoTrackRef.current =
+									await createLocalVideoTrack().catch((err) => {
+										alert(err);
+									});
+								if (localVideoTrackRef.current) {
+									room.localParticipant.publishTrack(
+										localVideoTrackRef.current
+									);
+								}
+							}
+							setLocalVideoTrack(localVideoTrackRef.current);
+						},
+					},
+					{
+						tab: "end",
+						icon: <Exit />,
+						onClick: () => {
+							room?.disconnect();
+							send("DISCONNECT");
+						},
+					},
+					{ tab: "call", icon: <Phone /> },
+					{ tab: "controls", icon: <Controls /> },
+				]).map(function ({ tab, icon, onClick }, i) {
+					let key = `key_${i}`;
+					return (
+						<button key={key} onClick={onClick}>
+							{icon}
+						</button>
+					);
+				})}
+			</div>
+		</StageDiv>
+	);
 }
 
 function Participant({ participant, participantNumber, totalParticipants }) {
-  const { subscribedTracks, isLocal } = useParticipant(participant);
+	const { subscribedTracks, isLocal } = useParticipant(participant);
 
-  const [videoPub, setVideoPub] = useState();
+	const [videoPub, setVideoPub] = useState();
 
-  // const currentGridLayout = videoGridLayout[totalParticipants];
-  // console.log(currentGridLayout);
+	// const currentGridLayout = videoGridLayout[totalParticipants];
+	// console.log(currentGridLayout);
 
-  useEffect(() => {
-    let _pub;
-    subscribedTracks.forEach((pub) => {
-      if (pub.kind === "video") {
-        _pub = pub;
-      }
-      setVideoPub(_pub);
-    });
-  }, [subscribedTracks]);
+	useEffect(() => {
+		let _pub;
+		subscribedTracks.forEach((pub) => {
+			if (pub.kind === "video") {
+				_pub = pub;
+			}
+			setVideoPub(_pub);
+		});
+	}, [subscribedTracks]);
 
-  useEffect(() => {
-    if (videoPub?.setEnabled) {
-      videoPub?.setEnabled(true);
-    }
-  }, [videoPub]);
+	useEffect(() => {
+		if (videoPub?.setEnabled) {
+			videoPub?.setEnabled(true);
+		}
+	}, [videoPub]);
 
-  return videoPub?.track ? (
-    <div
-      className={`participants ${
-        isLocal ? "local-participant" : "remote-participant"
-      }`}
-    >
-      <VideoRenderer track={videoPub.track} />
-      <br />
-      <span>{participant.identity}</span>
-    </div>
-  ) : (
-    <div
-      className={`participants ${
-        isLocal ? "local-participant" : "remote-participant"
-      }`}
-    >
-      <br />
+	return videoPub?.track ? (
+		<div
+			className={`participants ${
+				isLocal ? "local-participant" : "remote-participant"
+			}`}
+		>
+			<VideoRenderer track={videoPub.track} />
+			<br />
+			<span>{participant.identity}</span>
+		</div>
+	) : (
+		<div
+			className={`participants ${
+				isLocal ? "local-participant" : "remote-participant"
+			}`}
+		>
+			<br />
 
-      <label className="participantNumber">{participantNumber}</label>
-      <span>{participant.identity}</span>
-    </div>
-  );
+			<label className="participantNumber">{participantNumber}</label>
+			<span>{participant.identity}</span>
+		</div>
+	);
 }
