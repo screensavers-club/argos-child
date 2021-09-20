17ecf6ce1aef3c0f7687f14e61f2b6a7c0dbef50
Siah <i@siah.sg>
2021-09-20 16:41:46 +0800

    feat: default video fullscreen layout

--- src/argos-child-machine.js
+++ src/argos-child-machine.js
@@ -1,115 +1,109 @@
 import { createMachine, assign } from "xstate";
 
 let argosChildMachine = createMachine({
-  id: "ArgosChild",
-  initial: "get_identity",
+	id: "ArgosChild",
+	initial: "get_identity",
 
-  context: {
-    room: {},
-    nickname: "",
-    joining_room: null,
-    error: {},
-    identity: null,
-    current_layout: {
-      type: "D",
-      slots: [
-        { size: [50, 100], position: [0, 0], track: null },
-        { size: [50, 50], position: [50, 0], track: null },
-        { size: [50, 50], position: [50, 50], track: null },
-      ],
-    },
-  },
+	context: {
+		room: {},
+		nickname: "",
+		joining_room: null,
+		error: {},
+		identity: null,
+		current_layout: {
+			type: "A",
+			slots: [{ size: [100, 100], position: [0, 0], track: null }],
+		},
+	},
 
-  states: {
-    get_identity: {
-      on: {
-        IDENTITY: {
-          target: "list_rooms",
-          actions: assign({
-            identity: (context, event) => {
-              return event.identity;
-            },
-          }),
-        },
-      },
-    },
+	states: {
+		get_identity: {
+			on: {
+				IDENTITY: {
+					target: "list_rooms",
+					actions: assign({
+						identity: (context, event) => {
+							return event.identity;
+						},
+					}),
+				},
+			},
+		},
 
-    list_rooms: {
-      on: {
-        REQUEST_JOIN_ROOM: {
-          target: "enter_password",
-          actions: assign({
-            joining_room: (context, event) => {
-              return event.name;
-            },
-          }),
-        },
-      },
-    },
+		list_rooms: {
+			on: {
+				REQUEST_JOIN_ROOM: {
+					target: "enter_password",
+					actions: assign({
+						joining_room: (context, event) => {
+							return event.name;
+						},
+					}),
+				},
+			},
+		},
 
-    show_error: {},
+		show_error: {},
 
-    enter_password: {
-      on: {
-        JOIN_ROOM_WITH_TOKEN: {
-          target: "enter_nickname",
-          actions: assign({
-            room: (context, event) => {
-              return event.room;
-            },
-            token: (context, event) => {
-              return event.token;
-            },
-          }),
-        },
-      },
-    },
+		enter_password: {
+			on: {
+				JOIN_ROOM_WITH_TOKEN: {
+					target: "enter_nickname",
+					actions: assign({
+						room: (context, event) => {
+							return event.room;
+						},
+						token: (context, event) => {
+							return event.token;
+						},
+					}),
+				},
+			},
+		},
 
-    enter_nickname: {
-      on: {
-        ENTER_STAGE: {
-          target: "stage",
-          actions: assign({
-            nickname: (context, event) => {
-              return event.nickname;
-            },
-          }),
-        },
-      },
-    },
+		enter_nickname: {
+			on: {
+				ENTER_STAGE: {
+					target: "stage",
+					actions: assign({
+						nickname: (context, event) => {
+							return event.nickname;
+						},
+					}),
+				},
+			},
+		},
 
-    stage: {
-      on: {
-        INIT_LAYOUT_WITH_SELF: {
-          actions: assign({
-            current_layout: (context, event) => {
-              return {
-                type: "D",
-                slots: [
-                  { size: [50, 100], position: [0, 0], track: event.sid },
-                  { size: [50, 50], position: [50, 0], track: null },
-                  { size: [50, 50], position: [50, 50], track: null },
-                ],
-              };
-            },
-          }),
-        },
-        UPDATE_LAYOUT: {
-          actions: assign({
-            current_layout: (context, event) => {
-              console.log("updateLayout", event.layout);
-              return event.layout;
-            },
-          }),
-        },
-        DISCONNECT: { target: "list_rooms", room: "Not Connected" },
-      },
-    },
-  },
+		stage: {
+			on: {
+				INIT_LAYOUT_WITH_SELF: {
+					actions: assign({
+						current_layout: (context, event) => {
+							return {
+								type: "A",
+								slots: [
+									{ size: [100, 100], position: [0, 0], track: event.sid },
+								],
+							};
+						},
+					}),
+				},
+				UPDATE_LAYOUT: {
+					actions: assign({
+						current_layout: (context, event) => {
+							console.log("updateLayout", event.layout);
+							return event.layout;
+						},
+					}),
+				},
+				DISCONNECT: { target: "list_rooms", room: "Not Connected" },
+			},
+		},
+	},
 
-  on: {
-    BACK: { target: "list_rooms", room: "Not Connected" },
-  },
+	on: {
+		BACK: { target: "list_rooms", room: "Not Connected" },
+	},
 });
 
 export default argosChildMachine;
