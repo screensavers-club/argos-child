9b2d0ce143a23bf8d1e341d3c096bac33ac19484
Siah <i@siah.sg>
2021-09-22 10:45:22 +0800

    feat: try higher framerate

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ function App() {
 	return (
 		<div className="App" id="App">
 			<AppFrame>
-				<StatusBar room={_.get(state, "context.room.name")} version="0.2.1" />
+				<StatusBar room={_.get(state, "context.room.name")} version="0.2.2" />
 				<Screen state={state.value} context={state.context} send={send} />
 			</AppFrame>
 		</div>
