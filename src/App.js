a1d47c91b8154fce4609edfbfe8b44594ec54aba
Siah Tiong Hong <siah@goodwork.sg>
2022-04-18 13:31:08 +0800

    feat: 73

--- src/App.js
+++ src/App.js
@@ -31,7 +31,7 @@ function App() {
 			<AppFrame>
 				<StatusBar
 					room={_.get(state, "context.room.name")}
-					version="0.72"
+					version="0.73"
 					context={state.context}
 				/>
 				<Screen state={state.value} context={state.context} send={send} />
